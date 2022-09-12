23c622f6fd46a861e6d8fb9b7000c7f2ac58c839
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-12 10:00:34 +0200

    Step 7 - show movies (with MovieCard)

--- index.jsx
+++ index.jsx
@@ -14,9 +14,38 @@ function FrontPage(){
 
 }
 
+const MOVIES = [
+    {
+        title: "Plan 9 from outer space",
+        year: 1957,
+        plot: "utter confusion"
+    },
+    {
+        title: "Lecture 2 - Web applications",
+        year: 2022,
+        plot: "also confusion, but more modern"
+    },
+]
+
+function MovieCard({movie}){
+    const {title, year, plot} = movie;
+
+    return <div>
+        <h2> {title} - ({year}) </h2>
+        <p> {plot} </p>
+    </div>;
+}
+
+function ListMovies() {
+    return <div>
+        <h1> Movie List </h1>
+        {MOVIES.map(movie => <MovieCard key={movie.title} movie={movie} />)}
+    </div>;
+}
+
 function MovieApplication() {
     return <Routes>
-            <Route path={"/"} element={<h1>This is where the movies go</h1>}/>
+            <Route path={"/"} element={<ListMovies />}/>
             <Route path={"/new"} element={<h1>This is where you'll add something new</h1>}/>
         </Routes>
 }
